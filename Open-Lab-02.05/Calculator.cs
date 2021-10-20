bddc371715bde8270f3c38ef86bc3b903bfdcf0c
Peterkoricar <korkosk123@gmail.com>
2021-10-20 08:12:37 +0200

    The rest of the number

--- Open-Lab-02.05/Calculator.cs
+++ Open-Lab-02.05/Calculator.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._05
 {
-    public class Calculator
-    {
-        public bool DivisibleByFive(int number)
-        {
-            throw new NotImplementedException();
-        }
-    }
+  public class Calculator
+  {
+    public bool DivisibleByFive(int number)
+                   {
+                       if (number % 5 == 0)
+
+                      { 
+                       return true;
+                      }
+                      else
+                         {
+        return false;
+}
+}
+}
 }
